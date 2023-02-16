cd81ba3bb8f5617dd7bb9d194ebead258e75d6c2
Trần Quang Đãng <tranquangdang21@gmail.com>
2023-02-17 00:15:52 +0700

    full

--- TH/Lab 3/action_page.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (empty($_POST['name']) || empty($_POST['email'])) {
-  echo "Please fill all the fields";
-} else {
-  $name = $_POST['name'];
-  $email = $_POST['pwd'];
-  echo "Name: " . $name . " Email: " . $email;
-}
